0a7916c4ad994ecabadfbf23eecc387fdd054621
ctgk <r1135nj54w@gmail.com>
2020-10-23 19:30:30 +0900

    documenting Bayesian logistic regression model

--- bayesian/linear/_classifier.py
+++ bayesian/linear/_classifier.py
@@ -4,16 +4,56 @@ from bayesian._model import Model
 
 
 class Classifier(Model):
-    """
+    r"""
     # Bayesian Logistic Regression
 
     ### Prior distribution of model parameter
 
+    $$
+    p({\boldsymbol w} | \alpha)
+        = \mathcal{N}({\boldsymbol 0}, \alpha^{-1}{\bf I})
+    $$
+
     ### Likelihood function of model parameter
+
+    $$
+    \begin{aligned}
+        p({\boldsymbol y}|{\boldsymbol w}, {\bf X})
+            &= \prod_i p(y_i|{\boldsymbol w}, {\boldsymbol x}_i)\\
+        &= \prod_i {\rm Bern}(y_i
+            |{\boldsymbol w}^{\top}{\boldsymbol x}_i)
+    \end{aligned}
+    $$
 
     ### Posterior distribution of model parameter
 
+    $$
+    \begin{aligned}
+        p({\boldsymbol w}|{\boldsymbol y},{\bf X},\alpha)
+            &\propto p({\boldsymbol y}|{\boldsymbol w}, {\bf X})
+                p({\boldsymbol w} | \alpha)\\
+        &\approx \mathcal{N}(
+            {\boldsymbol w}|{\boldsymbol w}_{\rm MAP}, {\bf S}_N)\\
+    \end{aligned}
+    $$
+
+    Using the Laplace approximation of the posterior distribution,
+    ${\bf S}_N$ is the Hessian matrix of the log posterior distribution at
+    ${\boldsymbol w} = {\boldsymbol w}_{\rm MAP}$.
+
     ### Predictive distribution
+
+    $$
+    \begin{aligned}
+        p(y|{\boldsymbol x}, {\bf y}, {\bf X}, \alpha)
+            &= \int p(y|{\boldsymbol w},{\boldsymbol x})
+                p({\boldsymbol w}|{\boldsymbol y},{\bf X},\alpha)
+                    {\rm d}{\boldsymbol w}\\
+            &\approx \int {\rm Bern}(y|{\boldsymbol w},{\boldsymbol x})
+                \mathcal{N}({\boldsymbol w}|{\boldsymbol m}_{\rm MAP},
+                    {\bf S}_N){\rm d}{\boldsymbol w}\\
+    \end{aligned}
+    $$
     """
 
     def __init__(self, alpha: float, feature=None):
